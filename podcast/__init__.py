01fe2dd8fb13de6ce64d53d331956bacf8d198f6
Isaac Zhanyang Lin <ilin908@aucklanduni.ac.nz>
2024-08-20 21:34:57 +1200

    Completed routes and blueprints, rendered HTML pages but broken CSS.

--- podcast/__init__.py
+++ podcast/__init__.py
@@ -1,34 +1,74 @@
-"""Initialize Flask app."""
-
 from flask import Flask, render_template
-from podcast.PodcastDetails_routes import podcast_bp
-
-# TODO: Access to the podcast should be implemented via the repository pattern and using blueprints, so this can not
-#  stay here!
-from podcast.domainmodel.model import Podcast, Author
 
 
-# TODO: Access to the podcast should be implemented via the repository pattern and using blueprints, so this can not
-#  stay here!
-def create_some_podcast():
-    some_author = Author(1, "TED")
-    some_podcast = Podcast(66, some_author, "TED Talks Daily")
-    some_podcast.description = "Want TED Talks on the go? Every weekday, this feed brings you our latest talks in audio format. Hear thought-provoking ideas on every subject imaginable -- from Artificial Intelligence to Zoology, and everything in between -- given by the world's leading thinkers and doers. This collection of talks, given at TED and TEDx conferences around the globe, is also available in video format."
-    some_podcast.image_url = "http://is4.mzstatic.com/image/thumb/Music128/v4/d5/c6/50/d5c65035-505e-b006-48e5-be3f0f8f19f8/source/600x600bb.jpg"
-    return some_podcast
+def create_podcasts():
+    list_of_podcasts = [{
+        'title': 'D-Hour Radio Network',
+        'img_url': 'http://is3.mzstatic.com/image/thumb/Music118/v4/b9/ed/86/b9ed8603-d94b-28c5-5f95-8b7061bf22fa/source/600x600bb.jpg',
+        'id': 1,
+        'author': 'D Hour Radio Network',
+        'language': 'English',
+        'about': 'The D-Hour Radio Network is the home of real entertainment radio and "THE" premiere online radio network. We showcase dynamically dynamite radio shows for the sole purpose of entertaining your listening ear. Here on the D-hour Show Radio network we take pride in providing an outlet for Celebrity Artists, Underground Artists, Indie Artists, Producers, Entertainers, Entrepreneurs, Internet Stars and future business owners. We discuss topics of all forms and have a great time while doing so. We play all your favorite hits in the forms of Celebrity, Indie, Hip Hop, Soul/R&B, Pop, and everything else you want and consider popular. If you would like yourself and or your music to be showcased on our radio network submit email requests for music airplay, interviews and etc.. to: dhourshow@gmail.com and we will get back to you promptly. Here at the D-Hour Radio Network we are Family and all of our guests, listeners and loyal fans are family too. So tune into the D-Hour Radio Network and join the Family!	',
+        'categories': 'Society & Culture | Personal Journals',
+        'website': 'http://www.blogtalkradio.com/dhourshow',
+        'itunes_id': '538283940'
+    },
+        {
+            'title': 'Brian Denny Radio',
+            'img_url': 'http://is5.mzstatic.com/image/thumb/Music111/v4/49/c8/19/49c8190a-ca0f-f32c-c089-d7ae502d2cb8/source/600x600bb.jpg',
+            'id': 2,
+            'author': 'Brian Denny',
+            'language': 'English',
+            'about': '5-in-1: Brian Denny Radio is the fastest podcast in all the land. Each episode is 5 minutes and done in 1 take. Brian covers news, politics, sports, pro wrestling, life & more! No one does it faster or better, probably. #BDR	',
+            'categories': 'Professional | News & Politics | Sports & Recreation | Comedy',
+            'website': 'http://thebdshow.libsyn.com/podcast	',
+            'itunes_id': '1132261215'
+        },
+        {
+            'title': 'Onde Road - Radio Popolare',
+            'img_url': 'http://is2.mzstatic.com/image/thumb/Music62/v4/a5/44/ce/a544ce1f-3250-7147-1e8a-6316a161900c/source/600x600bb.jpg',
+            'id': 3,
+            'author': 'Radio Popolare',
+            'language': 'Italian',
+            'about': 'Il podcast la trasmissione Onde Road di Radio Popolare',
+            'categories': 'Society & Culture',
+            'website': 'http://www.radiopopolare.it',
+            'itunes_id': '568005832'
+        },
+        {
+            'title': 'Tallin Messages',
+            'img_url': 'http://is3.mzstatic.com/image/thumb/Music71/v4/d6/7a/a2/d67aa202-4c97-70d3-e629-b830567cff78/source/600x600bb.jpg',
+            'id': 4,
+            'author': 'Tallin Country Church',
+            'language': 'English',
+            'about': 'Podcast by Tallin Country Church',
+            'categories': 'Religion & Spirituality',
+            'website': 'http://soundcloud.com/tallin-church',
+            'itunes_id': '1165994461'
+        },
+        {
+            'title': 'Bethel Presbyterian Church (EPC) Sermons',
+            'img_url': 'http://is2.mzstatic.com/image/thumb/Music71/v4/0b/a0/ee/0ba0ee27-7137-fc7e-3bd2-4e07531c6b29/source/600x600bb.jpg',
+            'id': 5,
+            'author': 'Eric Toohey',
+            'language': 'English',
+            'about': 'Listen to sermons from Bethel Presbyterian Church (EPC) located just outside of Washington, PA.  For more information about Bethel, please visit our website at bethelepchurch.org.',
+            'categories': 'Religion & Spirituality | Christianity   ',
+            'website': 'http://www.bethelepchurch.org',
+            'itunes_id': '1120152718'
+        }
+    ]
+    return list_of_podcasts
 
 
 def create_app():
-    """Construct the core application."""
-
-    # Create the Flask app object.
     app = Flask(__name__)
-    app.register_blueprint(podcast_bp)
 
-    @app.route('/')
-    def home():
-        some_podcast = create_some_podcast()
-        # Use Jinja to customize a predefined html page rendering the layout for showing a single podcast.
-        return render_template('podcastDescription.html', podcast=some_podcast)
+    with app.app_context():
+        from .home import home
+        app.register_blueprint(home.home_blueprint)
+
+        from .podcasts import podcasts
+        app.register_blueprint(podcasts.podcasts_blueprint)
 
     return app
---
+++ podcast/podcasts/podcasts.py
@@ -0,0 +1,77 @@
+import os
+from flask import Blueprint, render_template, request, url_for, redirect
+import math
+from podcast.adapters.datareader.csvdatareader import CSVDataReader
+from podcast.adapters.interface_repository import MemoryPodcastRepository
+import random
+
+podcasts_blueprint = Blueprint('podcasts_bp', __name__)
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+podcasts_csv_path = os.path.join(base_dir, '..', 'adapters', 'data', 'podcasts.csv')
+episodes_csv_path = os.path.join(base_dir, '..', 'adapters', 'data', 'episodes.csv')
+
+csv_reader = CSVDataReader(podcasts_csv_path, episodes_csv_path)
+repository = MemoryPodcastRepository(csv_reader)
+
+@podcasts_blueprint.route('/podcasts', methods=['GET'])
+def home():
+    current_page = int(request.args.get('page', 1))
+    all_podcasts = repository.get_all_podcasts()
+
+    podcasts_per_page = 6
+    total_podcasts = len(all_podcasts)
+    total_pages = math.ceil(total_podcasts / podcasts_per_page)
+    
+    start_index = (current_page - 1) * podcasts_per_page
+    end_index = start_index + podcasts_per_page
+
+    podcasts_on_page = all_podcasts[start_index:end_index]
+
+    return render_template(
+        'catalogue.html', 
+        podcasts=podcasts_on_page, 
+        current_page=current_page, 
+        total_pages=total_pages
+    )
+
+
+@podcasts_blueprint.route('/description/<int:podcast_id>', methods=['GET'])
+def show_description(podcast_id):
+    # Retrieve the specific podcast by ID
+    podcast = repository.get_podcast_by_id(podcast_id)
+    if podcast is None:
+        return "Podcast not found", 404
+
+    # Retrieve related podcasts (modify this logic based on your actual implementation)
+    related_podcasts = repository.get_related_podcasts_by_id(podcast_id)
+
+    # Determine current page and total pages for pagination (example logic)
+    episodes_per_page = 5
+    total_pages = (len(podcast.episodes) + episodes_per_page - 1) // episodes_per_page
+    current_page = int(request.args.get('page', 1))
+    
+    # Slice the episodes list based on the current page
+    start_idx = (current_page - 1) * episodes_per_page
+    end_idx = start_idx + episodes_per_page
+    displayed_episodes = podcast.episodes[start_idx:end_idx]
+
+    # Pass the podcast, displayed episodes, related podcasts, and pagination data to the template
+    return render_template(
+        'podcastDescription.html',
+        podcast=podcast,
+        episodes=displayed_episodes,
+        related_podcasts=related_podcasts,
+        current_page=current_page,
+        total_pages=total_pages
+    )
+
+@podcasts_blueprint.route('/change_batch/<int:podcast_id>', methods=['POST'])
+def change_batch(podcast_id):
+    # Logic to change the batch of related podcasts or any other action
+    # For simplicity, let's assume we're shuffling the related podcasts
+    related_podcasts = repository.get_related_podcasts_by_id(podcast_id)
+    random.shuffle(related_podcasts)
+
+    # Redirect back to the podcast description page
+    return redirect(url_for('podcasts_bp.show_description', podcast_id=podcast_id))
